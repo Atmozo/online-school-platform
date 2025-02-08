2db6a09243ffd09147d7d37655b907d30e02bb14
Atmozo <ashelymozorand@gmail.com>
2025-02-08 16:23:46 +0200

    feat: Update database connection settings and configure server port

--- server/index.js
+++ server/index.js
@@ -298,7 +298,7 @@ app.use((err, req, res, next) => {
 });
 
 //Start server
-const PORT = process.env.PORT ||3306;
+const PORT = process.env.PORT ||5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
