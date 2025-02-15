17ddcc938302d9e42794c4fdcecc203bb82d70ac
Ashely Mozorandi <ashelymozorand@gmail.com>
2025-02-15 08:37:09 +0200

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,15 +1,3 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = pool.promise();
-
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -18,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // Add this
+  port: process.env.DB_PORT, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
